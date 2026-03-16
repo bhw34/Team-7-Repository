ec3dfdf8efeb4d172ea7fd870c01969ffaf7d025
Jaeden Glace <jsg155@case.edu>
2026-03-16 14:59:24 -0400

    Updated Comments for Prototype 2

--- Prototype2/src/main.cpp
+++ Prototype2/src/main.cpp
@@ -1,29 +1,30 @@
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
+// This initializes the output pins for both the servos
+const int LATCH_SERVO_PIN = A1; // Latch Servo Pin
+const int DOOR_SERVO_PIN = A0; // Door Servo Pin
 
-const int LATCH_SERVO_PIN = A1; 
-const int DOOR_SERVO_PIN = A0; 
-
+// Initializes the servos
 Servo latchServo;
 Servo doorServo;
 
-int closeAngle = 0;
-int openAngle = 90;
+int closeAngle = 0; // Initializes int to store starting servo position
+int openAngle = 90; // Initializes int to store open servo position
 
 void setup() {
     Serial.begin(115200);
 
-    // Force the ESP32 to set aside independent hardware timers
+    // Set independent hardware timers
     ESP32PWM::allocateTimer(0);
     ESP32PWM::allocateTimer(1);
 
-    // 2. Set the frequency (Standard servos are 50Hz)
+    // Set the frequency to 50Hz
     latchServo.setPeriodHertz(50);
     doorServo.setPeriodHertz(50);
 
-    // 3. Attach using the full parameter list (Pin, Min, Max)
-    // The ESP32 often needs the 500-2400 microsecond range explicitly defined
+    // Attach using the full parameter list (Pin, Min, Max)
+    // Define the range for the ESP32 (500-2400 microseconds)
     if (!latchServo.attach(LATCH_SERVO_PIN, 500, 2400)) {
         Serial.println("Latch Servo failed to attach");
     }
@@ -31,15 +32,16 @@ void setup() {
         Serial.println("Door Servo failed to attach");
     }
 
+    // Write start position
     latchServo.write(closeAngle);
     doorServo.write(closeAngle);
 }
 
 void loop() {
-    delay(2000);
-    latchServo.write(openAngle); 
-    
-    delay(2000); 
-    doorServo.write(openAngle); 
+    delay(2000); // Delay, door cant open till latch is open
+    latchServo.write(openAngle); // turns the latch servo 90 degrees
+
+    delay(2000); // Sets delay for robot cleaning cycle
+    doorServo.write(openAngle); // turns the door servo 90 degrees
 
 }

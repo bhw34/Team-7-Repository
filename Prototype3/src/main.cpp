58e132a2f87de520cedb211f1c99291ed49d0962
bhw34 <bhw34@case.edu>
2026-03-16 14:38:28 -0400

    edit8

--- Prototype3/src/main.cpp
+++ Prototype3/src/main.cpp
@@ -1,16 +1,24 @@
+
+/*
+This code makes use of both the previous prototypes to implement an iterative more complete design.
+For this code the code from protype 2 will be used to open the doors after a certain time elapsed. Then when the 
+ir sensor is moved near an object, simulating the robot crossing the threshold, the door will close and a trigger 
+will be set to remember that this door has been opened and closed
+*/
 #include <Arduino.h>
 #include <ESP32Servo.h>
 
-// Define the pin numbers for the Infrared obstacle avoidance sensor
-const int sensorPin = A0;
+const int sensorPin = A2; // Configures the IR sensor pin
+bool doorPassed = false; // Trigger to set door condition
 
 void setup() {
   pinMode(sensorPin, INPUT);  // Set sensorPin as input
-  Serial.begin(115200);         // Start serial communication at 9600 baud rate
+  Serial.begin(115200);         // Start serial communication 
 }
 
 void loop() {
-  // Reads 0 and light turns on if object is detected
-  Serial.println(digitalRead(sensorPin));  // Read the digital value from the sensor and print it to the serial monitor
-  delay(50);
+  // If the sensor pin is equal to 1 close the door and set the trigger to true
+  if(digitalRead(sensorPin) == 1) {
+
+  }
 }

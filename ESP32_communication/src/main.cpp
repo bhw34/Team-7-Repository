702febdc76147bfeb981f33f021e0f7e1e52f435
jem-331 <jem331@case.edu>
2026-04-06 12:17:25 -0400

    modified:   ESP32_communication/platformio.ini 	modified:   ESP32_communication/src/main.cpp

--- ESP32_communication/src/main.cpp
+++ ESP32_communication/src/main.cpp
@@ -1,18 +1,28 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <esp_wifi.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void readMacAddress(){
+  uint8_t baseMac[6];
+  esp_err_t ret = esp_wifi_get_mac(WIFI_IF_STA, baseMac);
+  if (ret == ESP_OK) {
+    Serial.printf("%02x:%02x:%02x:%02x:%02x:%02x\n",
+                  baseMac[0], baseMac[1], baseMac[2],
+                  baseMac[3], baseMac[4], baseMac[5]);
+  } else {
+    Serial.println("Failed to read MAC address");
+  }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void setup(){
+  Serial.begin(115200);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  WiFi.mode(WIFI_STA);
+  WiFi.STA.begin();
+
+  Serial.print("[DEFAULT] ESP32 Board MAC Address: ");
+  readMacAddress();
+}
+ 
+void loop(){
+
 }

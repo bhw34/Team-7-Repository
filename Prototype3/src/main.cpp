8e52671aa29307eab7eb304bb1f0b70eb0f0f95f
jem-331 <jem331@case.edu>
2026-04-08 13:43:31 -0400

    new file:   Prototype3/.gitignore 	new file:   Prototype3/.vscode/extensions.json 	new file:   Prototype3/include/README 	new file:   Prototype3/lib/README 	new file:   Prototype3/platformio.ini 	new file:   Prototype3/src/main.cpp 	new file:   Prototype3/test/README

---
+++ Prototype3/src/main.cpp
@@ -0,0 +1,75 @@
+/*
+This protype opens the latch and door motor. This is done using 2 servos one for the latch, which opens first and one for the door which opens second.
+*/
+
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+
+// This initializes the output pins for both the servos
+const int LATCH_SERVO_PIN = A1; // Latch Servo Pin
+const int DOOR_SERVO_PIN = A0; // Door Servo Pin
+const int sensorPin = A2; // Define the pin numbers for the Infrared obstacle avoidance sensor
+
+// Initializes the servos
+Servo latchServo;
+Servo doorServo;
+
+int closeAngle = 0; // Initializes int to store starting servo position
+int openAngle = 90; // Initializes int to store open servo position
+
+bool flag = true;
+
+void setup() {
+    Serial.begin(115200);
+    pinMode(sensorPin, INPUT);  // Set sensorPin as input
+
+    // Set independent hardware timers
+    ESP32PWM::allocateTimer(0);
+    ESP32PWM::allocateTimer(1);
+
+    // Set the frequency to 50Hz
+    latchServo.setPeriodHertz(50);
+    doorServo.setPeriodHertz(50);
+    
+    // Attach using the full parameter list (Pin, Min, Max)
+    // Define the range for the ESP32 (500-2400 microseconds)
+    if (!latchServo.attach(LATCH_SERVO_PIN, 500, 2400)) {
+        Serial.println("Latch Servo failed to attach");
+    }
+    if (!doorServo.attach(DOOR_SERVO_PIN, 500, 2400)) {
+        Serial.println("Door Servo failed to attach");
+    }
+
+    // Write start position
+    latchServo.write(closeAngle);
+    doorServo.write(closeAngle);
+
+    // Init Serial Monitor
+  Serial.begin(115200);
+ 
+}
+
+void loop() {
+    if (flag) {
+        flag = false;
+        delay(5000); // Delay, door cant open till latch is open
+        latchServo.write(openAngle); // turns the latch servo 90 degrees
+
+        delay(2000); // Sets delay for robot cleaning cycle
+        doorServo.write(openAngle); // turns the door servo 90 degrees
+
+    }
+        Serial.println(digitalRead(sensorPin));  // Read the digital value from the sensor and print it to the serial monitor
+    if (digitalRead(sensorPin) == LOW) { // Check if the sensor detects an obstacle
+        // Serial.println("Robot passed door.");
+        delay(4000);
+        doorServo.write(closeAngle); // Close the door if an obstacle is detected
+        delay(2000); // Wait for a second before checking again
+        latchServo.write(closeAngle); // Close the latch if an obstacle is detected
+        flag = true;
+      
+     }
+
+    
+}
